9ffc7c6fa95cfa53b7c0cfbed6c46ba8e57785fb
timislearning <tim.mitzen@gmail.com>
2019-08-10 14:29:06 -0400

    mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
 // Your code goes here
+const home = document.querySelector(".main-navigation a:first-child");
+
+const makeColor = (event) => {
+   event.currentTarget.style.color = "green";
+}
+const changeBack = (event) => {
+   event.currentTarget.style.color = "black";
+}
+
+home.addEventListener("mouseover", makeColor);
+home.addEventListener("click" , changeBack);
+
+document.addEventListener("keydown", (event) => {
+if(event.key === "t"){
+   alert("you hit the Tim key")
+}else{
+   alert("you hit the wrong key")
+}
+});
+
+window.onload = (event) => {
+   alert("Who's page? Tim's Page!")
+}
+
+const intro = document.querySelector(".intro h2");
+
+intro.addEventListener("dblclick", (event) => {
+   event.currentTarget.style.color = "red";
+});
+
+const changeBtn = document.querySelector(".btn");
+changeBtn.textContent = "Move over me";
+changeBtn.addEventListener("mouseout",(event) =>{
+   event.currentTarget.style.backgroundColor = "yellow";
+});
+
+changeBtn.addEventListener("click",(event)=>{
+   event.currentTarget.style.backgroundColor = "#17A2B8";
+});
+
+const removeBtn = document.querySelector("#last");
+removeBtn.textContent = "Right Click";
+removeBtn.addEventListener("contextmenu", (event) => {
+   event.currentTarget.style.display = "none";
+});
+
+const mouseLeave = document.querySelector(".mouseLeave");
+mouseLeave.addEventListener("mouseleave",(event) =>{
+   event.currentTarget.style.width = "20%"
+});
+
+const mouseClick = document.querySelector(".mouseLeave");
+
+mouseLeave.addEventListener("click",(event)=>{
+   event.currentTarget.style.width = "100%";
+});
+
+const copy = document.querySelector(".footer")
+copy.addEventListener("copy",(event) =>{
+  alert("You copied me")
+})
+
+const select = document.querySelector(".select");
+
+select.addEventListener("auxclick", (event) =>{
+   alert("you selected me");
+});
